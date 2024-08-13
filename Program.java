ea19eb17c0b2a1996684b74465b3ad2f9415dab0
Nef <nefesmeralda@gmail.com>
2024-08-13 15:35:12 -0600

    WIP for Letter to color

--- Program.java
+++ Program.java
@@ -3,8 +3,13 @@ package password.manager;
 class Program {
 
     public static void main(String[] args) {
+    }
 
+    private static int LetterToColor(char c) {
+    	//dshjgkhsljdyhufskdhufijshiudhfj
+    sjhdkbgnskldrbusfeil fn ubghfl
+    sdgiudyklghijfk pasihgrboltidf
+      	jdhbfgknlxm
     }
 
 }
-
